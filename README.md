c85a6e877a0e83eb49102116a8cd3c90a7914610
seedoilz <seedoilz609@qq.com>
2023-08-17 14:12:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# interpark-ticket
+抢票python脚本
+代码目前比较定制化，就是抢seventeen的
---
+++ infodemo.py
@@ -0,0 +1,17 @@
+Email=""
+Password="I"
+# 票数
+SeatTotal = 1
+Name = ""
+Year = ""
+# 月份写两位数字
+Month = ""
+Day = ""
+PhoneNo = ""
+HpNo = ""
+CardNo1 = ""
+CardNo2 = ""
+CardNo3 = ""
+CardNo4 = ""
+ValidMonth = ""
+ValidYear = ""
